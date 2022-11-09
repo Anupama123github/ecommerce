c4a46be1a81e279c9a487a36fe4fff98c69e04ec
Anupama Budholiya <114357893+Anupama123github@users.noreply.github.com>
2022-11-10 01:46:54 +0530

    Add files via upload

---
+++ ecommerce_sanity_stripe-20221109T200629Z-001/ecommerce_sanity_stripe/components/FunctionClick.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function FunctionClick() {
+  return (
+    <button>Click</button>
+    <div>FunctionClick</div>
+  )
+}
+
+export default FunctionClick
---
+++ ecommerce_sanity_stripe-20221109T200629Z-001/ecommerce_sanity_stripe/pages/_app.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { Toaster } from "react-hot-toast";
+
+import { Layout } from "../components";
+import "../styles/globals.css";
+import { StateContext } from "../context/StateContext";
+import Script from "next/script";
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+
+      <Script id="google-analytics-script" strategy="lazyOnload">
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+        page_path: window.location.pathname,
+        });
+    `}
+      </Script>
+      <StateContext>
+        <Layout>
+          <Toaster />
+          <Component {...pageProps} />
+        </Layout>
+      </StateContext>
+    </>
+  );
+}
+
+export default MyApp;
